13db1a9d1a704b94b470136f7a41c49ab9745fac
vivek-79 <vivekroy5667@gmail.com>
2024-10-10 00:58:12 +0530

    pattern till 10

--- Patterns/3.cpp
+++ Patterns/3.cpp
@@ -30,9 +30,51 @@ void print5(int n){
     }
 }
 
+void print6(int n){
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
 
+void print7(int n){
+    for(int i=0;i<n;i++){
+        for(int k=0;k<n-i-1;k++){
+            cout<<" ";
+        }
+        for(int j=0;j<(2*i)+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print8(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<(2*n)-(2*i);k++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print9(int n){
+    for(int i=1;i<=2*n+1;i++){
+        int stars =i;
+        if(i>n) stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin>>n;
-    print5(n);
+    print9(n);
 }
