50dbdf971f843aa2aa843529f3bb943c3897143c
vivek-79 <vivekroy5667@gmail.com>
2024-10-14 21:30:22 +0530

    complete pattern

--- Patterns/3.cpp
+++ Patterns/3.cpp
@@ -178,9 +178,93 @@ void print17(int n){
     }
 }
 
+void print18(int n){
+    int space=0;
+    for(int i=0;i<n;i++){
+       for(int j=0;j<n-i;j++){
+        cout<<"*";
+       }
+       for(int j=0;j<space;j++){
+        cout<<" ";
+       }
+      for(int j=0;j<n-i;j++){
+        cout<<"*";
+       }
+       space=space+2;
+       cout<<endl;
+    }
+    space=2*n-2;
+    for(int i=1;i<=n;i++){
+       for(int j=0;j<i;j++){
+        cout<<"*";
+       }
+       for(int j=0;j<space;j++){
+        cout<<" ";
+       }
+      for(int j=0;j<i;j++){
+        cout<<"*";
+       }
+       space=space-2;
+       cout<<endl;
+    }
+    
+}
 
+void print19(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<2*n-2*i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<=n-i-1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<=2*i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<=n-i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print20(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 || j==0 || i==n-1 || j==n-1){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void print21(int n){
+    for(int i=0;i<2*n-1;i++){
+        for(int j=0;j<2*n-1;j++){
+            int top = i;
+            int bottom=2*n-2-i;
+            int left=j;
+            int right=2*n-2-j;
+            cout<<(n-min(min(top,bottom),min(left,right)));
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin>>n;
-    print17(n);
+    print21(n);
 }
